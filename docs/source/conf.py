22fc5b31c4505e5b9755e638cfc5f34a78b02549
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2023-11-29 18:14:31 +0100

    RLS: 0.7.0 defaults spec

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,10 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'RatesLib'
+project = 'rateslib'
 copyright = '2022, JHM Darbyshire'
 author = 'JHM Darbyshire'
-release = 'dev'
+release = '0.7.x'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
